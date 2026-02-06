6144626723828366d6a1bdc1e47476eccfb75458
virgilistrate <virgilistrate24@gmail.com>
2026-02-06 10:30:12 +0100

    create project and server

---
+++ src/main/java/virgilistrate/U5L5/U5L5Application.java
@@ -0,0 +1,13 @@
+package virgilistrate.U5L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/virgilistrate/U5L5/U5L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package virgilistrate.U5L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
